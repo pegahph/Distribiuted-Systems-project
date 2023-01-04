7e318b6a3b0177b3efcbf90c313050844f5d9126
amrmaryam <121875802+amrmaryam@users.noreply.github.com>
2023-01-04 20:33:02 +0330

    Add files via upload

--- app-dis.py
+++ app-dis.py
@@ -1,31 +1,34 @@
 import socket 
+total_node= int(input("Enter number of members: "))
+prob_gossip = int(input("With what probability do you want to communicate? (0.25|0.75|0.5)"))
+host = input("Enter Session IP Adress: ")
 
+mem_nei=int(total_node*prob_gossip)
 
-host = '192.168.10.1' #localhost
+ip_string = input(f"Enter the address of {mem_nei} neighbors (split with '&'): ")
 
+Packet_string=""
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, 55555))
-server.listen()
+while True:
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host, 55555))
+    server.listen()
 
+    print("Server is listening...")
 
-def receive():
-    while True:
-        client, address = server.accept()
-        print(f'Connected with {str(address)}')
+    #while 1:
+    clientsocket, address = server.accept()
+    print(f'Connected with {str(address)}')
 
-        client.send('NICK'.encode("ascii"))
-        nickname = client.recv(1024).decode("ascii")
-       ## nicknames.append(nickname)
-       ## clients.append(client)
+    Packet_string = clientsocket.recv(1024).decode()
 
-        print(f'Nickname of the client is {nickname}')
-        ##broadcast(f'{nickname} joined the chat!'.encode("ascii"))
-        client.send(f'Connected to the {host}'.encode("ascii"))
+    print(f"Node Received New Message: {Packet_string}")
+    neighbor_ip =[]
+    neighbor_ip=ip_string.split('&')
 
+    c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    for i in neighbor_ip:
+        c.connect((i, 55555))
+        c.send(Packet_string.encode())
+        print(f'Sending Packet to : {i}' )
 
-
-
-
-print("Server is listening...")
-receive()
--- app-dis2.py
+++ app-dis2.py
@@ -1,36 +1,12 @@
 import socket 
 
-host='192.168.10.2'
-nickname = input("Choose a nickname: ")
+host='192.168.10.254'
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('192.168.10.1', 55555))
 
+client.send("salam".encode())
 
-
-
-def receive():
-    while True:
-        try:
-            message = client.recv(1024).decode("ascii")
-            if message == "NICK":
-                client.send(nickname.encode("ascii"))
-            else:
-                print(message)
-        except:
-            print("An error occurred!")
-            client.close()
-            break
-
-def swich_to_server():
-
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((host, 55555))
-    server.listen()
-
-
-receive()
-swich_to_server()
 
 
 

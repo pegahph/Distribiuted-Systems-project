98ae86b5118e26a83eae9a38af70ccbabc2e7ca7
pegah <pegah.ph22@gmail.com>
2023-02-06 01:17:38 +0330

    fix chatService bug

--- chatservice_real.py
+++ chatservice_real.py
@@ -1,33 +1,53 @@
 import socket
 import threading
 import re
+import json
+import time
 
-Packet_string= ""
+Packet_string= {
+    "type": "",
+    "ip": ""
+}
 neighbor_ip =[]
 id_user=""
 ip_sender=""
-Packet_string_old=""
+Packet_string_old={
+    "type": "",
+    "ip": ""
+}
+packet_types = {
+    "CHECK_USER_ID": "CHECK_USER_ID",
+    "CHECK_USER_ID_RESPONSE": "CHECK_USER_ID_RESPONSE",
+    "MESSAGE": "MESSAGE"
+}
 
-host = input("Enter Your IP Adress: ")
+host = input("Enter Your IP Address: ")
 ip_string = input(f"Enter the address of neighbors (split with '&'): ")
-first_mem=input("Are you a firt member? (yes|no) ")
+first_mem=input("Are you a first member? (yes|no) ")
 neighbor_ip=ip_string.split('&')
 
 
-def id_check(packet_id):
-
+def id_check(packet):
+    global packet_types
     global ip_sender
     
-    ip_sender = re.search(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', packet_id)
-    packet_id =packet_id.replace(ip_sender.group(),"")
-
-    if "ID_USER" in packet_id:
-            packet_id = packet_id.replace("ID_USER","")
-            if packet_id == id_user:
-                packet_id="choose ANOTHER id: "
-                return packet_id
-            else:
-                return(f"\n{packet_id} joined.. ")
+    # ip_sender = re.search(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', packet_id)
+    ip_sender = packet["ip"]
+    # packet_id =packet_id.replace(ip_sender.group(),"")
+    packet_id = packet["id"]
+    # packet_id = packet_id.replace("ID_USER","")
+    if packet_id == id_user:
+        packet={
+            "type": packet_types["CHECK_USER_ID_RESPONSE"],
+            "message": "choose ANOTHER id: ",
+            "receiver_ip": packet["ip"]
+        }
+        return packet
+    else:
+        return({
+            "type": packet_types["MESSAGE"],
+            "message" :f"\n{packet_id} joined.. "
+            })
 
 
 
@@ -35,27 +55,32 @@ def th_server():
     global Packet_string
     global id_user
     global Packet_string_old
-    Packet_string_new=""
+    Packet_string_new={
+        "type": "",
+        "ip": ""
+    }
     
-    
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host,55555))
+    server.listen()
+
     while True:
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.bind((host,55555))
-        server.listen()
-
         clientsocket,addr = server.accept()
-        
-        Packet_string_new= clientsocket.recv(1024).decode()
-        if Packet_string_new != Packet_string_old :
+        Packet_string_new= json.loads(clientsocket.recv(1024).decode())
+        if Packet_string_new != Packet_string_old  :
             Packet_string=Packet_string_new
-            if Packet_string =="choose ANOTHER id: ":
+            if Packet_string["type"] == packet_types["CHECK_USER_ID_RESPONSE"]:
                 id_user = input("choose ANOTHER id: ")
-                Packet_string=f"\n{id_user} joined... "
+                Packet_string={
+                    "type": packet_types["MESSAGE"],
+                    "message" :f"\n{id_user} joined... ",
+                    "ip": Packet_string_new["ip"]
+                }
 
-            elif "ID_USER" in Packet_string:
+            elif Packet_string["type"] == packet_types["CHECK_USER_ID"] :
                 Packet_string=id_check(Packet_string)
             else:
-                print(f"{Packet_string}")
+                print(Packet_string["message"])
             Packet_string_old=Packet_string_new
 
 def th_client():
@@ -63,24 +88,35 @@ def th_client():
     global neighbor_ip
     global ip_sender
     
-    while True:
-         
-      if Packet_string != ""  :  
-        c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        if Packet_string =="choose ANOTHER id: ": 
-            c.connect((ip_sender.group(),55555))
-            c.send((Packet_string).encode()) 
-
-        
-        else:
-         for ip in neighbor_ip:
-                c.connect((ip,55555))
-                c.send((Packet_string).encode())
-                #print(f'Sending {Packet_string} to : {ip}' )
+    while True:    
+        if Packet_string["type"] != ""  :  
+            c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            if Packet_string["type"] == packet_types["CHECK_USER_ID_RESPONSE"]:
+                c.connect((Packet_string["receiver_ip"],55555))
+                c.send((json.dumps(Packet_string)).encode()) 
 
             
-        Packet_string= ""
+            else:
+                for ip in neighbor_ip:
+                    c.connect((ip,55555))
+                    c.send((json.dumps(Packet_string)).encode())
+                    #print(f'Sending {Packet_string} to : {ip}' )
+
+                
+            Packet_string= {
+                "type": "",
+                "ip": ""
+            }
+
+id_user = input("choose an ID: ")
+
+if first_mem=="no":
+#  Packet_string="ID_USER"+id_user+host
+    Packet_string={
+        "type": packet_types["CHECK_USER_ID"],
+        "id": id_user,
+        "ip": host
+    }
 
 client_thread = threading.Thread(target=th_client)
 client_thread.start()
@@ -88,14 +124,14 @@ client_thread.start()
 server_thread = threading.Thread(target=th_server)
 server_thread.start()
 
-id_user = input("choose an ID: ")
 
-if first_mem=="no":
- Packet_string="ID_USER"+id_user+host
- 
-
-'''
 while True:
-    Packet_string_old = f'{id_user}: {input("")}'
-    Packet_string=Packet_string_old
-'''
+    time.sleep(1)
+    if Packet_string["type"] == "":
+        new_message = input("")
+        Packet_string_old = {
+            "type": packet_types["MESSAGE"],
+            "ip": host,
+            "message": id_user + ":" + new_message
+        }
+        Packet_string=Packet_string_old
